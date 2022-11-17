b86cd496e08c779e96dcbce4c3d09089d8a680b8
Sankha Sil <A200173944@t000fb7278.fritz.box>
2022-11-17 10:35:54 +0100

    fixed routing for items

--- apps/my-app/src/app/app-routing.module.ts
+++ apps/my-app/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: 'home',
+  loadChildren: () => import('./modules/lazy-home/lazy-home.module').then(m => m.LazyHomeModule)
+},
+{
+  path: 'items',
+  loadChildren: () => import('./modules/lazy-items/lazy-items.module').then(m => m.LazyItemsModule)
+},
+{
+  path: 'items/:itemId',
+  loadChildren: () => import('./modules/lazy-items/modules/items/components/items/modules/lazy-item/lazy-item.module').then(m => m.LazyItemModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- apps/my-app/src/app/modules/lazy-items/lazy-items.module.ts
+++ apps/my-app/src/app/modules/lazy-items/lazy-items.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-
-import { ItemsComponent } from './modules/items/components/items/items.component';
 import { ItemsRoutingModule } from './modules/items/items-routing.module';
 import { ItemsModule } from './modules/items/items.module';
 
